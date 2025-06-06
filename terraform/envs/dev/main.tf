4f10e6cb77e58c8d679302be2e6f5f04231fb549
Camilin <cami.car.val@outlook.com>
2025-06-05 19:21:56 -0500

    fix: pdate pipelines

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
   
   backend "azurerm" {
-    storage_account_name = "tfstate1749154418"
+    storage_account_name = "tfstate1749162202"
     container_name       = "tfstate"
     resource_group_name  = "rg-terraform-state"
     key                  = "dev/terraform.tfstate"
