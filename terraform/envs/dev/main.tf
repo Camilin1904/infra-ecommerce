34cee41b3f4fc5db6644823cdb832b52aa90a5c3
Camilin <cami.car.val@outlook.com>
2025-06-06 19:07:39 -0500

    fix: update pipeline

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  skip_provider_registration = true # Recommended for CI/CD
 }
 
 # Conditionally create resource group
